1a0e63f6628b2f0d4792642665120ca7746294bd
SanaGithubUber <sanagithububer@gmail.com>
2025-01-30 22:11:22 +0400

    Removed Config.java from Git tracking

--- employee/src/employee/Config.java
+++
@@ -1,8 +0,0 @@
-package employee;
-
-public class Config {
-    public static final String JDBC_URL = "jdbc:sqlserver://LAPTOP-MVUAJO1D\\SQLEXPRESS;Database=sana;"
-            + "IntegratedSecurity=true;encrypt=true;trustServerCertificate=true";
-
-    public static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-}
